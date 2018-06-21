d6ba149b1a70d1b8856849011fac8464ed8e999f
Prabhu S <prabhu@virtusa.com>
2018-06-21 18:36:49 +0530

    Added navigation to login and signup pages

---
+++ src/app/registration/pages/interviewer-registration/interviewer-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interviewer-registration',
+  templateUrl: './interviewer-registration.component.html',
+  styleUrls: ['./interviewer-registration.component.css']
+})
+export class InterviewerRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
