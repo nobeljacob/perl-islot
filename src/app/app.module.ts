a4684609d43c6f8e40366b73352bdaf2586a595f
Mounika Parsi <mounikaparsi@virtusa.com>
2018-06-21 14:33:55 +0530

    created folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LandingPageComponent } from './registration/pages/landing-page/landing-page.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    LandingPageComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule
