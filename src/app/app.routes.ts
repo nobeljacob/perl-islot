ddcb9e1db0c47a18ebffad5e609b110f7d32f3fd
Prabhu S <prabhu@virtusa.com>
2018-06-21 16:07:59 +0530

    Added Route to Landing Page

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Route } from '@angular/router';
+
+export const routes: Route[] = [
+    {
+        path:'',
+        loadChildren: 'app/registration/registration.module#RegistrationModule'
+    }
+];
